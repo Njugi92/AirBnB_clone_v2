615cb90f18497dec76fe930ddce148cc236d7f31
Njugi92 <kenkaruma.kk@gmail.com>
2023-09-18 23:16:20 +0300

    update base_model

--- models/base_model.py
+++ models/base_model.py
@@ -59,7 +59,7 @@ class BaseModel:
         models.storage.save()
 
     def to_dict(self):
-        """It returns a dictionary containing all keys/values of the instance"""
+        """It returns dictionary containing keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
