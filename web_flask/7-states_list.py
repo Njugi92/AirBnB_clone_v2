6b7b490957d8c18fda128d4947c16e051bc84ec9
Njugi92 <kenkaruma.kk@gmail.com>
2023-10-22 01:18:33 +0300

    Task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
-""" Start flask web application"""
+"""
+This starts a Flask web application
+"""
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-# Creates an instance of flask class and assigns it to variable app
 app = Flask(__name__)
 
 
-# Tear down app context to remove current sqlalchemy session after each request
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
 @app.teardown_appcontext
-def teardown(exception):
-    """remove current sqlalchemy session """
+def teardown_db(exception):
+    """This closes previous the storage on teardown"""
     storage.close()
 
-
-# Defines the route for '/states_list'
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """ Displays aHTML page with a list of all states objects in DBstorage
-    States are sorted by name
-    """
-    states = sorted(storage.all(state).values(), key=lambda s: s.name)
-    # render the template and pass the list of states to the template
-    return render_template("7-states_list.html", states=states)
-
-
-if __name__ == "__main__":
-    # Start flask development server
-    # Listen on all available network interfaces (0.0.0.0) and port 5000
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
